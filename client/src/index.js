e271e565864515eb85e25abfd5b6fa89a3282332
Mary <mariyapiligrim3@gmail.com>
2022-04-30 23:23:20 +0300

    Create context provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
+
+export const Context = createContext(null);
 
 ReactDOM.render(
-    <App />,
-document.getElementById('root')
+  <Context.Provider value={{ user: new UserStore(), device: new DeviceStore() }}>
+    <App />
+  </Context.Provider>,
+  document.getElementById("root")
 );
-
-
