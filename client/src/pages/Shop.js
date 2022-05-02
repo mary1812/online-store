d9bbda56f5f8cd1d2acc11799632df4d5b92e271
Mary <mariyapiligrim3@gmail.com>
2022-05-02 21:59:08 +0300

    Add container

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Container} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import TypeBar from '../components/TypeBar';
+import BrandBar from '../components/BrandBar'
 
 const Shop = () => {
   return (
-    <div>
-      SHOP
-    </div>
+    <Container>
+      <Row className="mt-4">
+        <Col md={3}>
+          <TypeBar/>
+        </Col>
+        <Col md={9}>
+          <BrandBar/>
+        </Col>
+        
+      </Row>
+    </Container>
   );
 }
 
