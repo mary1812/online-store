ea2ea05594423287b020d169e034f3d96605462e
Mary <mariyapiligrim3@gmail.com>
2022-05-02 20:08:33 +0300

    Add some styles

--- client/src/components/NavBarMenu.js
+++ client/src/components/NavBarMenu.js
@@ -3,19 +3,31 @@ import { Context } from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
+import { SHOP_ROUTE } from "../utils/constants";
+import { Button } from "react-bootstrap";
+import { observer } from "mobx-react-lite";
+import { Container } from "react-bootstrap";
 
-const NavBarMenu = () => {
+const NavBarMenu = observer(() => {
   const { user } = useContext(Context);
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <NavLink href="#home">Home</NavLink>
-        <NavLink href="#features">Features</NavLink>
-        <NavLink href="#pricing">Pricing</NavLink>
-      </Nav>
+      <Container>
+      <NavLink to={SHOP_ROUTE}>LYCHEE LINGERIE</NavLink>
+      {user.isAuth ? (
+        <Nav className="ms-auto">
+          <Button>Админ панель</Button>
+          <Button className="ms-2">Выйти</Button>
+        </Nav>
+      ) : (
+        <Nav className="ms-auto">
+          <Button onClick={()=> user.setIsAuth(true)}>Авторизация</Button>
+        </Nav>
+      )}
+      </Container>
+      
     </Navbar>
   );
-};
+});
 
 export default NavBarMenu;
