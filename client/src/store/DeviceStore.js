e96d366507f43e149b2dca3a4ad2fb015bbb42fe
Mary <mariyapiligrim3@gmail.com>
2022-05-02 21:57:58 +0300

    Add some brands

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -2,13 +2,20 @@ import {makeAutoObservable} from 'mobx';
 
 export default class DeviceStore{
   constructor(){
+    this._selectedBrand=""
     this._types=[
       {id: 1, name: 'Холодильники'},
-      {id: 2, name: 'Смартфоны'}
+      {id: 2, name: 'Смартфоны'},
+      {id: 3, name: 'Ноутбуки'},
+      {id: 4, name: 'Телевизоры'},
     ]
     this._brands =[
       {id:1, name:'Samsung'},
-      {id:1, name:'iPhone'}
+      {id:2, name:'iPhone'},
+      {id:3, name:'Lenovo'},
+      {id:4, name:'Xiaomi'},
+      {id:5, name:'Meizu'}
+
     ]
     this._devices=[
       {id:1, name:'13pro', price: 150000, rating: 5, img:'https://applehome.te.ua/wp-content/uploads/2021/09/iphone-13-pro-max-graphite-select.png'},
@@ -17,7 +24,9 @@ export default class DeviceStore{
       {id:4, name:'13pro', price: 150000, rating: 5, img:'https://applehome.te.ua/wp-content/uploads/2021/09/iphone-13-pro-max-graphite-select.png'},
       {id:5, name:'13pro', price: 150000, rating: 5, img:'https://applehome.te.ua/wp-content/uploads/2021/09/iphone-13-pro-max-graphite-select.png'},
     ]
+    this._selectedType = {}
     makeAutoObservable(this)
+
   }
 
   setTypes(types){
@@ -29,6 +38,12 @@ export default class DeviceStore{
   setDevices(devices){
     this._devices = devices
   }
+  setSelectedType(type){
+    this._selectedType = type
+  }
+  setSelectedBrand(brand){
+    this._selectedBrand = brand
+  }
 
   get types(){
     return this._types
@@ -38,5 +53,11 @@ export default class DeviceStore{
   }
   get devices(){
     return this._devices
+  }
+  get selectedType(){
+    return this._selectedType
+  }
+  get selectedBrand(){
+    return this._selectedBrand
   }
 }
